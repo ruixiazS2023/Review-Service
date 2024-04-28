80c1fcfad189659aec97a031f69650eaf8dc1c5a
ruixiazS2023 <ruixiaz@andrew.cmu.edu>
2024-04-27 17:21:58 -0700

    delete TODO

--- src/main/java/com/sda/project/controller/ReviewController.java
+++ src/main/java/com/sda/project/controller/ReviewController.java
@@ -10,10 +10,7 @@ import com.sda.project.request.UndoRequest;
 import com.sda.project.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
 import java.sql.Timestamp;
-import java.util.List;
 
 @RestController
 @CrossOrigin
