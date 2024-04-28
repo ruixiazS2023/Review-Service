36efb8e9037e3cd74c54626fa6471c1b98e8347e
ruixiazS2023 <ruixiaz@andrew.cmu.edu>
2024-04-27 17:21:12 -0700

    delete TODO

--- src/main/java/com/sda/project/controller/ReviewController.java
+++ src/main/java/com/sda/project/controller/ReviewController.java
@@ -10,11 +10,13 @@ import com.sda.project.request.UndoRequest;
 import com.sda.project.service.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 @RequestMapping("/reviews")
 public class ReviewController {
 
